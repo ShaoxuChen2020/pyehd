553038ce76ea86a5ec403280f93c025746ac8b3d
ShaoxuChen2020 <70495213+ShaoxuChen2020@users.noreply.github.com>
2020-10-04 21:50:46 -0400

    Update pyehd.py
    真的不行吗，赵兄？

--- pyehd.py
+++ pyehd.py
@@ -5,17 +5,17 @@ from PIL import Image
 def findehd(img):
     r,c,m = np.shape(img)
     if m==3:
-        img = Image.open(img).convert('LA') # convert RGB img to grayscale img
-    M = 4*np.ceil(r/4) 
+        img = img.convert('LA') # convert RGB img to grayscale img
+    M = 4*np.ceil(r/4)
     N = 4*np.ceil(c/4)
     img = Image.fromarray(img)
     img.resize(size=(M, N))
     AllBins = np.zeros((17, 5))
     p = 1
     L = 0
-    for i in range(1,4,1):
+    for i in range(0,3,1):
         K = 0
-        for j in range(1,4,1):
+        for j in range(0,3,1):
             block = img[K+1:K+M/4, L+1:L+N/4]
             AllBins[p,:] = getbins(double(block))
             K = K + (M/4)
@@ -41,7 +41,7 @@ def getbins(imgb):
     D135 = np.array([0,1.414],[-1.414,0]) # diagonal 135 edge operator
     Isot = np.array([2,-2],[-2,2]) # isotropic edge operator
     T = 50 # threshold
-    
+
     nobr = M/2 # loop limits
     nobc = N/2 # loop limits
     L = 0
@@ -57,7 +57,7 @@ def getbins(imgb):
             pd135 = np.abs(np.sum(np.sum(block*D135)))
             pisot = np.abs(np.sum(np.sum(block*Isot)))
             parray = [pv,ph,pd45,pd135,pisot]
-            index = np.argmax(parray) 
+            index = np.argmax(parray)
             value = parray[index]
             if value >= T:
                 bins[index]=bins[index]+1 # update bins values
